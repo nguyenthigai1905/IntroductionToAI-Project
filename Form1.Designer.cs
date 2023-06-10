5d29cda67e020faf7cae5898945c9803f8fed60d
Nguyen Thi Gai <100947712+nguyenthigai1905@users.noreply.github.com>
2023-06-10 20:17:17 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace TikiCrawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_url = new System.Windows.Forms.Label();
+            this.lbl_limit = new System.Windows.Forms.Label();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.txt_limit = new System.Windows.Forms.TextBox();
+            this.btn_getData = new System.Windows.Forms.Button();
+            this.txt_res = new System.Windows.Forms.TextBox();
+            this.btn_copy = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_url
+            // 
+            this.lbl_url.AutoSize = true;
+            this.lbl_url.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_url.Location = new System.Drawing.Point(44, 14);
+            this.lbl_url.Name = "lbl_url";
+            this.lbl_url.Size = new System.Drawing.Size(167, 29);
+            this.lbl_url.TabIndex = 0;
+            this.lbl_url.Text = "Link sản phẩm";
+            // 
+            // lbl_limit
+            // 
+            this.lbl_limit.AutoSize = true;
+            this.lbl_limit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_limit.Location = new System.Drawing.Point(44, 281);
+            this.lbl_limit.Name = "lbl_limit";
+            this.lbl_limit.Size = new System.Drawing.Size(279, 29);
+            this.lbl_limit.TabIndex = 1;
+            this.lbl_limit.Text = "Số lượng comment tối đa";
+            // 
+            // txt_url
+            // 
+            this.txt_url.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_url.Location = new System.Drawing.Point(49, 54);
+            this.txt_url.Multiline = true;
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(357, 215);
+            this.txt_url.TabIndex = 2;
+            // 
+            // txt_limit
+            // 
+            this.txt_limit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_limit.Location = new System.Drawing.Point(329, 275);
+            this.txt_limit.Name = "txt_limit";
+            this.txt_limit.Size = new System.Drawing.Size(77, 35);
+            this.txt_limit.TabIndex = 3;
+            this.txt_limit.Text = "10";
+            // 
+            // btn_getData
+            // 
+            this.btn_getData.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_getData.Location = new System.Drawing.Point(49, 328);
+            this.btn_getData.Name = "btn_getData";
+            this.btn_getData.Size = new System.Drawing.Size(357, 94);
+            this.btn_getData.TabIndex = 4;
+            this.btn_getData.Text = "LẤY DỮ LIỆU";
+            this.btn_getData.UseVisualStyleBackColor = true;
+            this.btn_getData.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // txt_res
+            // 
+            this.txt_res.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_res.Location = new System.Drawing.Point(444, 54);
+            this.txt_res.Multiline = true;
+            this.txt_res.Name = "txt_res";
+            this.txt_res.Size = new System.Drawing.Size(416, 256);
+            this.txt_res.TabIndex = 5;
+            // 
+            // btn_copy
+            // 
+            this.btn_copy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_copy.Location = new System.Drawing.Point(444, 328);
+            this.btn_copy.Name = "btn_copy";
+            this.btn_copy.Size = new System.Drawing.Size(200, 94);
+            this.btn_copy.TabIndex = 6;
+            this.btn_copy.Text = "COPY";
+            this.btn_copy.UseVisualStyleBackColor = true;
+            this.btn_copy.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.IndianRed;
+            this.btn_exit.FlatAppearance.BorderSize = 0;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Location = new System.Drawing.Point(861, 0);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(52, 43);
+            this.btn_exit.TabIndex = 7;
+            this.btn_exit.Text = "X";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save.Location = new System.Drawing.Point(660, 328);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(200, 94);
+            this.btn_save.TabIndex = 12;
+            this.btn_save.Text = "LƯU FILE";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(439, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Dữ liệu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(912, 438);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_copy);
+            this.Controls.Add(this.txt_res);
+            this.Controls.Add(this.btn_getData);
+            this.Controls.Add(this.txt_limit);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.lbl_limit);
+            this.Controls.Add(this.lbl_url);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Shopee";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_url;
+        private System.Windows.Forms.Label lbl_limit;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.TextBox txt_limit;
+        private System.Windows.Forms.Button btn_getData;
+        private System.Windows.Forms.TextBox txt_res;
+        private System.Windows.Forms.Button btn_copy;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.IO;
+using xNet;
+using Newtonsoft.Json.Linq;
+
+namespace TikiCrawler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int MODE = 0;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void getMode()
+        {
+            string url = txt_url.Text;
+            if (url.Length < 20) return;
+            for (int i = 0; i <= 10; ++i)
+            {
+                string tmp = "";
+                for (int j = i; j < i + 4; ++j)
+                    tmp += url[j];
+                if (tmp == "tiki")
+                {
+                    MODE = 0;
+                    return;
+                }
+            }
+            MODE = 1;
+        }
+        private string getIdTiki()
+        {
+            string tmp = txt_url.Text;
+            string res0 = "", res = "";
+            int pos = 0;
+            for (int i = tmp.Length - 4; i > 0; --i)
+            {
+                string temp = "";
+                for (int j = i; j <= i + 3; ++j)
+                    temp += tmp[j];
+                if (temp == "html")
+                {
+                    pos = i - 2;
+                    break;
+                }
+
+            }
+            while (tmp[pos] != 'p')
+            {
+                res0 = res0 + tmp[pos];
+                --pos;
+            }
+            for (int i = res0.Length - 1; i >= 0; --i)
+                res += res0[i];
+            return res;
+        }
+
+        private string getIdShopee()
+        {
+            string tmp = txt_url.Text + "?";
+            string res = "";
+            int pos = 0;
+            for (int i = 20; i < tmp.Length; ++i)
+            {
+                string tmp0 = "" + tmp[i] + tmp[i + 1];
+                if (tmp0 == "i.")
+                {
+                    pos = i + 2;
+                    break;
+                }
+            }    
+            while (tmp[pos]!='?')
+            {
+                res += tmp[pos];
+                ++pos;
+            }
+            return res;
+        }
+
+        private void getDataTiki()
+        {
+            string URL = "https://tiki.vn/api/v2/reviews?limit=" + txt_limit.Text + "&include=comments,contribute_info&sort=score%7Cdesc,id%7Cdesc,stars%7Call&page=1&product_id=" + getIdTiki();
+            HttpRequest http = new HttpRequest();
+            string html = http.Get(URL).ToString();
+            txt_res.Text = html;
+        }
+
+        private void getDataShopee()
+        {
+            string shopId = "", productId = "";
+            string tmp = getIdShopee();
+            int pos = 0;
+            while (tmp[pos] != '.')
+            {
+                shopId += tmp[pos];
+                pos++;
+            }
+            ++pos;
+            for (int i = pos; i < tmp.Length; ++i)
+                productId += tmp[i];
+            int lim = int.Parse(txt_limit.Text);
+            if (lim < 50)
+            {
+                string URL = "https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&itemid=" + productId
+                    + "&limit=" + txt_limit.Text + "&shopid=" + shopId + "&type=0";
+                HttpRequest http = new HttpRequest();
+                string html = http.Get(URL).ToString();
+                JObject jss = JObject.Parse(html);
+                JArray temparray = (JArray)jss["data"]["ratings"];
+                txt_res.Text = temparray.ToString();
+            }
+            else
+            {
+                int t = lim / 50;
+                JArray jArray = new JArray();
+                for (int i = 0; i < t; ++i)
+                {
+                    int offset = i * 50;
+                    string URL = "https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&itemid=" + productId
+                    + "&limit=50&offset=" + offset.ToString() + "&shopid=" + shopId + "&type=0";
+                    HttpRequest http = new HttpRequest();
+                    string html = http.Get(URL).ToString();
+                    JObject jss = JObject.Parse(html);
+                    JArray temparray = (JArray)jss["data"]["ratings"];
+                    jArray.Merge(temparray);
+                }
+                txt_res.Text = jArray.ToString();
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                getMode();
+            }
+            catch
+            {
+                txt_res.Text = "URL hoặc số comment sai định dạng";
+            }
+            if (MODE == 0)
+            {
+                try
+                {
+                    getDataTiki();
+                }
+                catch
+                {
+                    txt_res.Text = "URL hoặc số comment sai định dạng";
+                }
+            }
+            else
+            {
+                try
+                {
+                    getDataShopee();
+                }
+                catch
+                {
+                    txt_res.Text = "URL hoặc số comment sai định dạng";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // copy dữ liệu
+        {
+            Clipboard.SetText(txt_res.Text);
+        }  
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e) //kéo thả di chuyển ứng dụng
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // tắt ứng dụng
+        {
+            this.Close();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e) // lưu file
+        {
+            string dataFilePath = System.AppDomain.CurrentDomain.BaseDirectory + "data.json";
+            File.WriteAllText(dataFilePath, txt_res.Text);
+        }
+    }
+}
